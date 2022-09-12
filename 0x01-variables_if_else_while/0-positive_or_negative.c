5b13642f5c827cb204dfa1ef2764aff5f63b2dd0
SamuelOtisi <samuelotisikalu@gmai.com>
2022-09-12 11:08:22 -0700

    edit postive negative zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,9 +10,9 @@
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
- *    
- * Return: Always 0.   
- */
+ *
+ * Return: Always 0.
+*/
 int main(void)
 {
 	int n;
@@ -22,6 +22,7 @@ int main(void)
 
 	if (n > 0)
 		printf("%d is positive\n", n);
+	
 	else if (n < 0)							printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
