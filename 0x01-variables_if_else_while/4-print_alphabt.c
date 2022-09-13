10bec4b9317f0b33995667d800120abf953d2237
SamuelOtisi <samuelotisikalu@gmai.com>
2022-09-12 23:54:14 -0700

    added 5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,24 @@
 /*
- * File: 2-print_alphabet.c
+ * File: 4-print_alphabt.c
  * Auth: Samuel Otisi
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints the alphabet in lowercase, except for q and e.
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
 
 	putchar('\n');
 
