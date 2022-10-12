0a0a823e623947966e720ecdcf265ddd514a53c0
SamuelOtisi <samuelotisikalu@gmai.com>
2022-10-11 19:46:44 -0700

    edited 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,20 +1,22 @@
+/*
+ * File: 2-main.c
+ * Auth: Samuel Otisi
+ */
 
-#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main - Entry Point
- * Return: 0
+ * main - Prints the name of the file of the program
+ *        was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
 	printf("%s\n", __FILE__);
+
 	return (0);
 }
-
-
-
-
 
 
 
